3ce50bb20c9ed457c8e6465de0fa774fe728e87f
Teemu Tiljander <teemutil@gmail.com>
2022-09-26 05:14:34 +0300

    More heroku config

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-
+from os import getenv
 
 
 app = Flask(__name__)
 
+uri = getenv("DATABSE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://teemu:asd@localhost"
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 db = SQLAlchemy(app)
